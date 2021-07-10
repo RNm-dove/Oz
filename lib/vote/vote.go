93282adb31532c512d6ca7981b5480d5cde30e4d
RNm-dove <m.rynk.plume@gmail.com>
2021-07-10 23:23:50 +0900

    master  #time Add vote outline module

---
+++ lib/vote/vote.go
@@ -0,0 +1,18 @@
+// vote は投票を扱うパッケージです
+package vote 
+
+import (
+	"time"
+	"vote/candidate"
+	"vote/voter"
+	"area"
+)
+
+struct Vote {
+	period: time
+	candidates: []candidate.Candidate
+}
+
+func main()  {
+	
+}
